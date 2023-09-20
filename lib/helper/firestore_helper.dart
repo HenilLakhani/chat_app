40683513cc75ebec4d08999cacc3a66a83696949
henil lakhani <lakhanihenil113@gmail.com>
2023-09-20 23:53:55 +0530

    20/09/2023

---
+++ lib/helper/firestore_helper.dart
@@ -0,0 +1,10 @@
+import '';
+
+class FireStoreHelper {
+  FireStoreHelper._();
+  static final FireStoreHelper fireStoreHelper = FireStoreHelper._();
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  String collection = "Student";
+}
