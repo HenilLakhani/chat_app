74fe7594f05a235e7e3df1d42074d4e249623177
henil lakhani <lakhanihenil113@gmail.com>
2023-10-04 18:08:38 +0530

    20/09/2023, 24/09/2023,4/10/2023

--- lib/view/screens/login_page.dart
+++ lib/view/screens/login_page.dart
@@ -1,27 +1,50 @@
+import 'package:chat_app/controller/Text_Controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _editingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.red,
+              Colors.blue,
+              Colors.purpleAccent,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
               children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                )
+                const SizedBox(height: 30),
+                reusableTextField("Enter UserName", Icons.person_outline, false,
+                    _editingController),
+                const SizedBox(height: 20),
+                reusableTextField("Enter Password", Icons.lock_outline, true,
+                    _editingController),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
